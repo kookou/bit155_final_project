53ab76fe85667390d2c370bb717fffb50d89b2b3
hrin <khrin126@naver.com>
2020-07-10 19:56:12 +0900

    aside 진행중...,.,

---
+++ src/main/java/kr/or/bit3004/aside/AllBoardList.java
@@ -0,0 +1,15 @@
+package kr.or.bit3004.aside;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AllBoardList {
+	private int allBoardListNo;
+	private String name;
+	private int teamNo;
+	private int boardTypeNo;
+}
