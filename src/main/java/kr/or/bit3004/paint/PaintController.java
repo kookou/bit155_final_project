b4e9f7d03947c69701c0d519de6ec62e96d2d7e7
hi240 <240__@naver.com>
2020-07-16 12:33:06 +0900

    그림판 그럴싸하게 레이아웃에 넣기

--- src/main/java/kr/or/bit3004/paint/PaintController.java
+++ src/main/java/kr/or/bit3004/paint/PaintController.java
@@ -2,17 +2,26 @@ package kr.or.bit3004.paint;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import kr.or.bit3004.aside.AsideService;
+
 @Controller	
 public class PaintController {
 
+	@Autowired
+	private AsideService asideService;
+	
 	@RequestMapping("/paint.do")
-	public String chat(Model model, RedirectAttributes rttr, HttpSession session) {
+	public String chat(Model model, RedirectAttributes rttr, HttpSession session,int teamNo) {
 		if(session.getAttribute("id")==null) {
+			model.addAttribute("team", asideService.getTeam(teamNo));
+			model.addAttribute("teamMember", asideService.getTeamMember(teamNo));
+			model.addAttribute("allBoardList", asideService.getAllBoardList(teamNo));
 			rttr.addFlashAttribute("msg","You need to login first");
 			System.out.println("로그인해야..");
 			
@@ -20,7 +29,9 @@ public class PaintController {
 			return "paint/paint";
 		}
 		
-	
+		model.addAttribute("team", asideService.getTeam(teamNo));
+		model.addAttribute("teamMember", asideService.getTeamMember(teamNo));
+		model.addAttribute("allBoardList", asideService.getAllBoardList(teamNo));
 		
 		return "paint/paint";
 	}
