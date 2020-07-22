09a609152a7d595c9d7dd281a8f65eb5049055ad
Jinwon <selfish7@naver.com>
2020-07-22 20:43:22 +0900

    ┌ⓢⓤⓜⓜⓔⓡⓝⓞⓣⓔ┘여전히 제자리 걸음

--- src/main/java/kr/or/bit3004/config/WebMvcConfig.java
+++ src/main/java/kr/or/bit3004/config/WebMvcConfig.java
@@ -12,6 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     //로 접속하면 C:/summernote_image/1234.jpg 파일을 불러온다.
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/summernoteImage/**").addResourceLocations("file://C:/summernote_image/");
+		registry.addResourceHandler("/summernoteImage/**")
+				.addResourceLocations("file://C:/summernote_image/");
     }
 }
