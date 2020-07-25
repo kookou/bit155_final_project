49a4921b1fc42940686b4c774990b9056a24da1b
geranium236 <geranium236@gmail.com>
2020-07-25 18:15:39 +0900

    principal 대신 session값 사용 (소셜로그인 getName 값 다름) / 차트 완성

--- src/main/java/kr/or/bit3004/dao/TimelineDao.java
+++ src/main/java/kr/or/bit3004/dao/TimelineDao.java
@@ -12,12 +12,15 @@ public interface TimelineDao {
 	// 팀별 전체 to do 수 구하기
 	public int countTotalTodos(int teamNo);
 		
-	// 팀별 전체 칸반카드 수  구하기
+	// 팀별 전체 칸반카드 수  구하기(view 사용)
 	public int countTotalKanbanCards(int teamNo);
 		
-	// 팀별 전체 게시물 수 구하기
+	// 팀별 전체 게시물 수 구하기(view 사용)
 	public int countTotalBoards(int teamNo);
-		
+	
+	// 팀별 전체 게시물(칸반카드 + 게시글) 수 구하기(view 사용) 
+	public int countTotalPosts(int teamNo);
+	
 	// 팀별 전체 업로드 파일 수 구하기
 	public int countTotalUploadFiles(int teamNo);
 	
@@ -29,9 +32,13 @@ public interface TimelineDao {
 	// 팀별 오늘 작성된 게시글 수 구하기	
 	public int countTodaysNewBoards(int teamNo);
 	
+	// 팀원별 전체 작성한 게시물 수 구하기(view 사용)
+	public List<ChartData> countTotalPostsByMember(int teamNo);
+	
 	
 	//팀별 전체 TODO 수, 완료된 TODO 수 구하기
 	public List<ChartData> getTodoCountByState(int teamNo);
+	
 	
 	
 }
