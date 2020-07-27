089c6679a0bdebb04371c9e7267b737c13ca1117
kookou <nnaangee@gmail.com>
2020-07-27 17:27:49 +0900

    팀메인 오류 확인

--- src/main/resources/static/js/groupAndTeam.js
+++ src/main/resources/static/js/groupAndTeam.js
@@ -235,7 +235,7 @@ function makeListHtml(resData) {
 			html +=				'<div class="user-img rounded-circle" style="float: left; background-color: white; overflow: hidden; height: 40px; width: 40px;">'
 	       	html +=				'<div style="top: 0; left: 0; right: 0; bottom: 0;">'
 			html += 				'<img src="assets/images/userImage/'+obj3.image+'" alt="user"';
-			html += 					'style="width: auto; height: 60px; transform: translate(-20%, -16%); display:block;"';
+			html += 					'style="width: auto; height: 55px; transform: translate(-20%, -16%); display:block;"';
 			html += 					'data-toggle="tooltip" data-placement="top" title="'+obj3.id+'">';
        		html += 			'</span>';
 			}
@@ -318,7 +318,7 @@ $('#createTeamBtn').click(function() {
 			html +=				'<div class="user-img rounded-circle" style="float: left; background-color: white; overflow: hidden; height: 40px; width: 40px;">'
 		    html +=				'<div style="top: 0; left: 0; right: 0; bottom: 0;">'
 		    html += 				'<img src="assets/images/userImage/'+currUserImage+'" alt="user"';
-			html += 					'style="width: auto; height: 60px; transform: translate(-20%, -16%); display:block;"';
+			html += 					'style="width: auto; height: 55px; transform: translate(-20%, -16%); display:block;"';
 			html += 					'data-toggle="tooltip" data-placement="top" title="'+currUser+'">';
        		html += 			'</span>';
 			html += 		'</div>';
